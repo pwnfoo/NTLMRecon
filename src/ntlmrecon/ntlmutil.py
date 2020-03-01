052a4b88a15bf1fd1828ce625c6bc74309051887
Sachin Kamath <sskamath96@gmail.com>
2020-03-01 19:35:16 +0530

    Add force mode to replace files and add /ecp/ CVE note
    Signed-off-by: Sachin Kamath <sskamath96@gmail.com>

--- src/ntlmrecon/ntlmutil.py
+++ src/ntlmrecon/ntlmutil.py
@@ -188,11 +188,13 @@ def detect_ntlm_auth(url):
                 if 'WWW-Authenticate' in response_headers.keys():
                     if 'NTLM' in response_headers['WWW-Authenticate']:
                         print(colored("[+] {} has NTLM authentication enabled!".format(url), 'green'))
+                        if '/ecp/' in url.lower():
+                            print(colored("  [!] /ecp/ endpoint might be vulnerable to CVE-2020-0688."
+                                          " Try running https://github.com/Ridter/cve-2020-0688", "red"))
                         return True
                     else:
-                        print("[+] {} requires authentication but the method was found to be {}".format(
-                            url, response_headers['WWW-Authenticate']
-                        ))
+                        print(colored("[+] {} requires authentication but the method was found to be {}".format(
+                                url, response_headers['WWW-Authenticate']), "yellow"))
             else:
                 return False
 
